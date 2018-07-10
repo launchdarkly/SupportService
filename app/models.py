6ee7e0c4c0a017909657ff0b5ebc8262b210f2be
manuelPartida <manuel@launchdarkly.com>
2018-07-10 10:35:47 -0700

    configuring circleci

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    beta_tester = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
