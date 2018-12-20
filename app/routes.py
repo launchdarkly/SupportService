b6a28612af74e2711cf810725d259218678a8953
Lev Lazinskiy <lev@launchdarkly.com>
2018-12-20 14:37:55 -0800

    login when registering

--- app/routes.py
+++ app/routes.py
@@ -103,7 +103,8 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('core.login'))
+        login_user(user)
+        return redirect(url_for('core.index'))
     return render_template('beta/auth/register.html', title='Support Request')
 
 @core.route('/login', methods=['GET', 'POST'])
