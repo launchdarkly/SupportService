98cf322eec7b678be814b04507e097aed67f2d1f
Lev Lazinskiy <lev@launchdarkly.com>
2019-01-29 17:40:46 -0800

    explicitly tag docker with SHA1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,4 +25,5 @@ ssh -o StrictHostKeyChecking=no $PROD_SERVER 'docker system prune --force --volu
 # Log into Production Server, Pull and Restart Docker
 ssh -o StrictHostKeyChecking=no $PROD_SERVER 'cd /var/www/app && docker-compose pull'
 ssh -o StrictHostKeyChecking=no $PROD_SERVER 'cd /var/www/app && docker-compose build'
-ssh -o StrictHostKeyChecking=no $PROD_SERVER 'cd /var/www/app && docker-compose restart'
+ssh -o StrictHostKeyChecking=no $PROD_SERVER 'cd /var/www/app && docker-compose up -d'
+ssh -o StrictHostKeyChecking=no $PROD_SERVER 'cd /var/www/app && docker-compose restart web'
