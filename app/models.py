b69f280a142df4aad615e9596555041a07e3f6d2
Dan O'Brien <dobrien@launchdarkly.com>
2019-09-23 19:05:55 -0400

    remove unneeded import

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 from faker import Faker
 from flask import current_app
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import AnonymousUserMixin, UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
