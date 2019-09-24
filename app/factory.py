d88b2df4d49d663d1edb3c42c2b06f85f4dd713a
Dan O'Brien <dobrien@launchdarkly.com>
2019-09-24 09:47:38 -0400

    refactor for db upgrade

--- app/factory.py
+++ app/factory.py
@@ -76,7 +76,7 @@ class SubdomainDispatcher(object):
         return app(environ, start_response)
 
 
-def create_app(subdomain, env, config_name):
+def create_app(env_id, env_api_key, config_name):
     """Flask application factory.
 
     :param config_name: Flask Configuration
@@ -86,7 +86,7 @@ def create_app(subdomain, env, config_name):
     :returns: a flask application
     """
     app = Flask(__name__)
-    app = build_environments(subdomain, env, app)
+    app = build_environment(env_id, env_api_key, app)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
@@ -136,20 +136,20 @@ def make_app(ld, rclient, subdomain, project, config_name):
     project = pickle.loads(rclient.get(PROJECT_NAME))
     for env in project.environments:
         if env.key == subdomain:
-            return create_app(subdomain, env, config_name=config_name)
+            return create_app(env.id, env.api_key, config_name)
 
     return NotFound()
 
-def build_environments(subdomain, env, app):
+def build_environment(env_id, env_api_key, app):
     if os.environ.get('TESTING') is None or os.environ.get('TESTING') == False:
         app.redis_client = FlaskRedis(app)
-        logging.info(env.api_key)
-        logging.info(env.id)
-        app.config['LD_CLIENT_KEY'] = env.api_key
-        app.config['LD_FRONTEND_KEY'] = env.id
+        logging.info(env_api_key)
+        logging.info(env_id)
+        app.config['LD_CLIENT_KEY'] = env_api_key
+        app.config['LD_FRONTEND_KEY'] = env_id
     else:
-        app.config['LD_CLIENT_KEY'] = env['LD_CLIENT_KEY']
-        app.config['LD_FRONTEND_KEY'] = env['LD_FRONTEND_KEY']
+        app.config['LD_CLIENT_KEY'] = env_id['LD_CLIENT_KEY']
+        app.config['LD_FRONTEND_KEY'] = env_api_key['LD_FRONTEND_KEY']
 
     return app
 
