bc7f91fab1fcbca71b9f6538e4d05502c1ea4c8a
Dan O'Brien <dobrien@launchdarkly.com>
2019-10-10 11:55:34 -0400

    deploy: Modify deployment to Swarm

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -20,8 +20,8 @@ scp -o StrictHostKeyChecking=no docker-compose.prod.yml $STAGING_SERVER:/var/www
 # Clean up old images
 ssh -o StrictHostKeyChecking=no $STAGING_SERVER 'docker system prune --force --volumes'
 
-# Log into Production Server, Pull and Restart Docker
-ssh -o StrictHostKeyChecking=no $STAGING_SERVER 'cd /var/www/app && docker-compose pull'
-ssh -o StrictHostKeyChecking=no $STAGING_SERVER 'cd /var/www/app && docker-compose build'
-ssh -o StrictHostKeyChecking=no $STAGING_SERVER 'cd /var/www/app && docker-compose down --remove-orphans'
-ssh -o StrictHostKeyChecking=no $STAGING_SERVER 'cd /var/www/app && docker-compose up -d'
+# Log into Staging Server, Pull and Restart Docker
+ssh -o StrictHostKeyChecking=no $PRODUCTION_SERVER 'cd /var/www/app && docker stack deploy -c docker-compose.yml support-service'
+ssh -o StrictHostKeyChecking=no $PRODUCTION_SERVER 'cd /var/www/app && docker-compose build'
+#ssh -o StrictHostKeyChecking=no $PRODUCTION_SERVER 'cd /var/www/app && docker-compose down --remove-orphans'
+ssh -o StrictHostKeyChecking=no $PRODUCTION_SERVER 'cd /var/www/app && docker-compose up --no-deps -d app'
